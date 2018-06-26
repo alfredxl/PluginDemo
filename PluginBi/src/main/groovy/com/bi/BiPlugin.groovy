e169e5c79262fcd37ea77fa3b47935b97ded976f
谢文良 <765947965@qq.com>
2018-06-26 14:25:33 +0800

    Initial commit

---
+++ PluginBi/src/main/groovy/com/bi/BiPlugin.groovy
@@ -0,0 +1,20 @@
+package com.bi
+
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * <br> ClassName:   ${className}* <br> Description:
+ * <br>
+ * <br> @author:      谢文良
+ * <br> Date:        2018/6/26 14:17
+ */
+class BiPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+        target.logger.info("Bi-Start--------------")
+        AppExtension android1 = target.extensions.getByType(AppExtension)
+        android1.registerTransform(new BiTransform(project))
+    }
+}
