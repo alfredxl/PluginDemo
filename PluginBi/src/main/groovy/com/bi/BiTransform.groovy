d0d19ea889d419ec2f004739b7fb4a571727add3
谢文良 <765947965@qq.com>
2018-06-29 11:54:28 +0800

    1

--- PluginBi/src/main/groovy/com/bi/BiTransform.groovy
+++ PluginBi/src/main/groovy/com/bi/BiTransform.groovy
@@ -10,6 +10,8 @@ import com.android.build.api.transform.TransformInput
 import com.android.build.api.transform.TransformInvocation
 import com.android.build.api.transform.TransformOutputProvider
 import com.android.build.gradle.internal.pipeline.TransformManager
+import javassist.ClassPool
+import javassist.CtClass
 import org.gradle.api.Project
 import com.android.build.api.transform.Format
 import org.apache.commons.io.FileUtils
@@ -44,10 +46,20 @@ class BiTransform extends Transform {
 
     @Override
     void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        ClassPool pool= ClassPool.getDefault()
+        CtClass c = pool.getCtClass("com.aspectj.demo.MainActivity")
+        for (int i = 0; i < c.declaredMethods.size(); i++) {
+            def method = c.declaredMethods[i]
+            println(method.name)
+            if (method.name.contains("toast")){
+                method.insertAfter("Toast.makeText(this, \"cccc\", Toast.LENGTH_SHORT).show();")
+                println("插入成功")//测试成功的插入代码
+            }
+        }
         super.transform(transformInvocation)
     }
 
-    @Override
+  /*  @Override
     void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
         // inputs就是输入文件的集合
         // outputProvider可以获取outputs的路径
@@ -63,16 +75,16 @@ class BiTransform extends Transform {
                 FileUtils.copyDirectory(directoryInput.file, dest)
             }
 
-/*            input.jarInputs.each { JarInput jarInput ->
+*//*            input.jarInputs.each { JarInput jarInput ->
                 //TODO 这里可以对input的文件做处理，比如代码注入！
-*//*                String jarPath = jarInput.file.absolutePath
+*//*/*                String jarPath = jarInput.file.absolutePath
                 String projectName = project.rootProject.name
                 if (jarPath.endsWith("classes.jar")
                         && jarPath.contains("exploded-aar\\" + projectName)//这里的路径在我的项目中并不存在, gradle版本不同可能已经不一样了
                         // hotpatch module是用来加载dex，无需注入代码
                         && !jarPath.contains("exploded-aar\\" + projectName + "\\hotpatch")) {
                     Inject.injectJar(jarPath)//调用对jar进行注入的方法
-                }*//*
+                }*//*/*
                 // 重命名输出文件（同目录copyFile会冲突）
                 def jarName = jarInput.name
                 def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
@@ -85,7 +97,7 @@ class BiTransform extends Transform {
                 dest.mkdirs()//需要先创建文件才可以哦
                 dest.createNewFile()
                 FileUtils.copyFile(jarInput.file, dest)
-            }*/
+            }*//*
         }
-    }
+    }*/
 }
