dd5dd798b4f54a3a2888e4fa1fd78a04f9f62279
谢文良 <765947965@qq.com>
2018-07-02 16:22:08 +0800

    1

--- PluginBi/src/main/groovy/com/bi/BiTransform.groovy
+++ PluginBi/src/main/groovy/com/bi/BiTransform.groovy
@@ -15,6 +15,7 @@ import javassist.CtClass
 import org.gradle.api.Project
 import com.android.build.api.transform.Format
 import org.apache.commons.io.FileUtils
+import org.apache.commons.codec.digest.DigestUtils
 
 
 class BiTransform extends Transform {
@@ -44,8 +45,7 @@ class BiTransform extends Transform {
         return false
     }
 
-
-   /* @Override
+    /*@Override
     void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
         // inputs就是输入文件的集合
         // outputProvider可以获取outputs的路径
@@ -86,4 +86,43 @@ class BiTransform extends Transform {
             }
         }
     }*/
+
+    @Override
+    void transform(Context context, Collection<TransformInput> inputs,
+                   Collection<TransformInput> referencedInputs,
+                   TransformOutputProvider outputProvider, boolean isIncremental)
+            throws IOException, TransformException, InterruptedException {
+        // Transform的inputs有两种类型，一种是目录，一种是jar包，要分开遍历
+        inputs.each { TransformInput input ->
+            //对类型为“文件夹”的input进行遍历
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+                //文件夹里面包含的是我们手写的类以及R.class、BuildConfig.class以及R$XXX.class等
+
+                // 获取output目录
+                def dest = outputProvider.getContentLocation(directoryInput.name,
+                        directoryInput.contentTypes, directoryInput.scopes,
+                        Format.DIRECTORY)
+
+                // 将input的目录复制到output指定目录
+                FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+            //对类型为jar文件的input进行遍历
+            input.jarInputs.each { JarInput jarInput ->
+
+                //jar文件一般是第三方依赖库jar文件
+
+                // 重命名输出文件（同目录copyFile会冲突）
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+                //生成输出路径
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                //将输入内容复制到输出
+                FileUtils.copyFile(jarInput.file, dest)
+            }
+        }
+    }
 }
